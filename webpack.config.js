da91e0dc855bacd102461d22e7a0aff43a3af045
Paco Preciado Mendoza <__pm@outlook.com>
2019-03-23 12:26:01 -0600

    :sparkles: finished

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,10 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  // mode: 'development', // Set the mode configuration option to development to make sure that the bundle is not minified
+  // optimization: {
+  //   usedExports: true
+  // }
+  mode: 'production' // the whole bundle is now minified
 };
